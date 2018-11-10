699b773b007ffa4d8699ebd1615d0b95f655e7a3
waura <waura0@yahoo.co.jp>
2018-11-10 16:17:15 +0900

    Implement thirdparty auth on backend

--- server/server/server.js
+++ server/server/server.js
@@ -5,6 +5,66 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+// Passport configurators..
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+var bodyParser = require('body-parser');
+//var flash      = require('express-flash');
+
+// attempt to build the providers/passport config
+var config = {};
+try {
+  config = require('../providers.json');
+} catch (err) {
+  console.trace(err);
+  process.exit(1); // fatal
+}
+
+app.middleware('parse', bodyParser.json());
+app.middleware('parse', bodyParser.urlencoded({
+  extended: true,
+}));
+
+// The access token is only available after boot
+app.middleware('auth', loopback.token({
+  model: app.models.accessToken,
+}));
+
+/*
+app.middleware('session:before', cookieParser(app.get('cookieSecret')));
+app.middleware('session', session({
+  secret: 'kitty',
+  saveUninitialized: true,
+  resave: true,
+}));
+*/
+passportConfigurator.init();
+
+// We need flash messages to see passport errors
+//app.use(flash());
+
+/*
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential,
+});
+*/
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
+
+/*
+app.get('/auth/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
+*/
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
