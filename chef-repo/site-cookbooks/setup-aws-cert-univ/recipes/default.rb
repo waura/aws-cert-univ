139e275c7c75c4f9bfecc249d899654b173927bc
waura <waura0@yahoo.co.jp>
2018-11-22 23:14:40 +0900

    Update chef recipe for staging

--- chef-repo/site-cookbooks/setup-aws-cert-univ/recipes/default.rb
+++ chef-repo/site-cookbooks/setup-aws-cert-univ/recipes/default.rb
@@ -4,7 +4,31 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-git "~/aws-cert-univ" do
+include_recipe "git::default"
+
+user "acu" do
+  home "/home/acu"
+  shell "/bin/bash"
+  password nil
+  action :create
+end
+
+group "acu" do
+  gid 1010
+  members ['acu']
+  action :create
+end
+
+directory "/var/www" do
+  owner "acu"
+  group "acu"
+  recursive true
+  mode 0755
+  action :create
+  not_if { File.exists? "/var/www" }
+end
+
+git "/var/www/aws-cert-univ" do
   repository "git://github.com/waura/aws-cert-univ.git"
   reference "master"
   action :sync
